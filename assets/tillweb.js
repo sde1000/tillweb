6902b6915e7cd56886ae7fcedec520fc530bd2c7
Stephen Early <steve@assorted.org.uk>
2026-02-22 19:18:17 +0000

    Remove jquery-ui
    jquery-ui was only used for its datepicker; quicktill.tillweb is now
    using browser native datepickers.

--- assets/tillweb.js
+++ assets/tillweb.js
@@ -4,11 +4,6 @@
 // jquery
 require("expose-loader?exposes=$,jQuery!jquery");
 
-// jquery-ui
-require('jquery-ui/themes/base/theme.css');
-require('jquery-ui/themes/base/datepicker.css');
-require('jquery-ui/ui/widgets/datepicker');
-
 // bootstrap
 require('bootstrap/dist/css/bootstrap.min.css');
 require('bootstrap');
